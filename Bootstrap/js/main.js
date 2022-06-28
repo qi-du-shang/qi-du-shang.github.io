b44954265d66db641ee0b2864e357166e896f83e
qi-du-shang <85401586+qi-du-shang@users.noreply.github.com>
2022-06-28 14:21:21 +0800

    Add files via upload

---
+++ Bootstrap/js/main.js
@@ -0,0 +1,12 @@
+//低版本jquery用的是$(window).load()函数，高版本用的是$(window).on()
+//加载动画 $(window).on('load', function handlePreloader() {
+	if ($('.preloader').length) {
+		$('.preloader').delay(200).fadeOut(500);
+	}
+});
+
+//置顶按钮
+let glyphiconChevronUp = document.querySelector('.glyphicon-chevron-up');
+glyphiconChevronUp.onclick = function() {
+	window.scrollTo(0, 0);
+}
---
+++ Bootstrap/js/writeText.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+ 	var myDiv = document.getElementById("writeSomething");
+ 	var contentArr = "这是一个博客,您分享生活,技术,文章等内容的乐园。".split("");
+ 	var content = "";
+ 	var index = 0;
+ 	var ID = setInterval(function() {
+ 		content += contentArr[index];
+ 		myDiv.innerHTML = content + "_";
+ 		index++;
+ 		if (index === contentArr.length) {
+ 			myDiv.innerHTML = content;
+ 			clearInterval(ID);
+ 			console.log("结束了");
+ 		}
+ 	}, 300);
+ }
