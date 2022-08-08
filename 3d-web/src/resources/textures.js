f0d634a197aea9875e4a01a377df3fa8115d4056
qi-du-shang <85401586+qi-du-shang@users.noreply.github.com>
2022-08-08 21:34:14 +0800

    Add files via upload

---
+++ 3d-web/src/resources/textures.js
@@ -0,0 +1,63 @@
+let billboardTextures = {};
+// 第一块展板图片地址
+billboardTextures.blogTexture = '../src/jsm/blog.png';
+// 第二块展板图片地址
+billboardTextures.indexTexture =
+  '../src/jsm/index.png';
+// 第三块展板图片地址
+billboardTextures.fundTexture =
+  '../src/jsm/fund.png';
+
+let boxTexture = {};
+// 链接的图标地址
+boxTexture.Github = '../src/jsm/githubLogo.png';
+boxTexture.QRcode = '../src/jsm/QRcode.png';
+boxTexture.QQ = '../src/jsm/qq.png';
+boxTexture.mail = '../src/jsm/envelope.png';
+boxTexture.reactIcon = '../src/jsm/react.png';//react的图片
+boxTexture.allSkills = '../src/jsm/allSkills.png';//所有技能的图片
+boxTexture.lensFlareMain = '../src/jsm/lensflare0.png';//太阳光
+boxTexture.skrillex = '../src/jsm/skrillex.png';//文本结束的署名
+boxTexture.edmText = '../src/jsm/EDM.png';//文本
+
+// 砖块材质
+let stoneTexture = '../src/jsm/stone.png';
+// 展板木头材质
+let woodTexture = '../src/jsm/woodTexture.jpg';
+
+// 文字图片地址
+let inputText = {};
+inputText.terpSolutionsText = '../src/jsm/terp-solutions-text.png';//第一个展板文本
+inputText.activities = '../src/jsm/activities_text.png';//wishes下的图片
+inputText.bagholderBetsText = '../src/jsm/bagholderbets-text.png';//第二个展板文本
+inputText.homeSweetHomeText = '../src/jsm/home-sweet-home-text.png';//第三个展板文本
+inputText.staticPortfolio = '../src/jsm/static-portfolio.png';//作品位置及访问方式提示文本
+inputText.pcControl = '../src/jsm/pc-control.png'//小球操控方式提示文本
+inputText.mobileControl = '../src/jsm/mobile-control.png'//手机浏览时才显示的小球操控提示文本
+inputText.link = '../src/jsm/link.png'//github,qq,qrcode,mail下的文本
+
+//SVG
+let SVG = {};
+SVG.reactLogo = '../src/jsm/#';
+
+// 链接的跳转地址
+let URL = {};
+URL.blog =
+  '#';
+URL.index = 'https://qi-du-shang.github.io';
+URL.fund = '#';
+URL.gitHub = 'https://github.com/qi-du-shang';
+URL.QRcode = '#';
+URL.email = 'http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=z6mjrryn-vr3--79_f-7_4__vuGsoKI';
+// URL.music = 'https://qi-du-shang.github.io';
+URL.devTo =
+  'http://wpa.qq.com/msgrd?v=3&uin=1580126044&site=qq&menu=yes"><img border="0" src="http://wpa.qq.com/pa?p=2:1580126044:41';
+
+export {
+  billboardTextures,
+  boxTexture,
+  inputText,
+  URL,
+  stoneTexture,
+  woodTexture,
+};
