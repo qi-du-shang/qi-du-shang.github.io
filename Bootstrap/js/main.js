cc84d6ce908b1e5a80bd81c054b2e71ec90552fb
qi-du-shang <85401586+qi-du-shang@users.noreply.github.com>
2023-02-02 14:28:11 +0800

    Update main.js

--- Bootstrap/js/main.js
+++ Bootstrap/js/main.js
@@ -1,9 +1,10 @@
 //低版本jquery用的是$(window).load()函数，高版本用的是$(window).on()
-//加载动画 $(window).on('load', function handlePreloader() {
-	if ($('.preloader').length) {
-		$('.preloader').delay(200).fadeOut(500);
-	}
-});
+//加载动画
+//  $(window).on('load', function handlePreloader() {
+// 	if ($('.preloader').length) {
+// 		$('.preloader').delay(200).fadeOut(500);
+// 	}
+// });
 
 //置顶按钮
 let glyphiconChevronUp = document.querySelector('.glyphicon-chevron-up');
